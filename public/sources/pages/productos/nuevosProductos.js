6f735afc9c0090c132731f2513c2dc292124bfc6
Katherine Hinojosa <geofisikatt@gmail.com>
2025-02-18 17:27:29 -0600

    creamos la funcion para validar datos ingresados al formulario y añadimos la clase ItemsController y el método addItem

---
+++ public/sources/pages/productos/nuevosProductos.js
@@ -0,0 +1,67 @@
+import { validateFields } from "./validations";
+import { addItem } from "./ItemsController";
+
+// Initialize a new TaskManager with currentId set to 0
+const itemsController = new ItemsController(0);
+
+// Select the New Task Form
+const ItemForm = document.querySelector('#newItemForm');
+
+// Add an 'onsubmit' event listener
+ItemForm.addEventListener('submit', (event) => {
+    // Prevent default action
+    event.preventDefault();
+
+    // Select the inputs
+    const ItemNameInput = document.querySelector('#producto');
+    const ItemDescription = document.querySelector('#descripcion_producto');
+    const ItemPrice = document.querySelector('#precio');
+    const ItemStock = document.querySelector('#stock');
+    const ItemReason = document.querySelector('#razon_compra');
+    const ItemInfo = document.querySelector('#informacion_produccion');
+    const ItemImage = document.querySelector('#imagen_producto');
+
+
+    // Get the values of the inputs
+    const name = ItemNameInput.value;
+    const stock = ItemStock.value;
+    const price = ItemPrice.value;
+
+    
+        /*
+        Validation code here
+    */
+   validateFields(name,price,stock);
+
+    // Add the task to the task manager
+    itemsController.addItem(name, stock, price);
+
+    // Clear the form
+    ItemNameInput.value = '';
+    ItemDescription.value = '';
+});
+
+
+
+
+function addItemCard(item){
+    const itemHTML = '<div class="card" style="width: 18rem;">\n' +
+        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+        '    <div class="card-body">\n' +
+        '        <h5 class="card-title">'+item.name+'</h5>\n' +
+        '        <p class="card-text">'+item.description+'</p>\n' +
+        '        <a href="#" class="btn btn-primary">Add</a>\n' +
+        '    </div>\n' +
+        '</div>\n' +
+        '<br/>';
+    const itemsContainer = document.getElementById("list-items");
+    itemsContainer.innerHTML += itemHTML;
+}
+
+addItemCard({'name':'juice',
+    'img':'https://www.gs1india.org/media/Juice_pack.jpg',
+    'description':'Orange and Apple juice fresh and delicious'});
+
+addItemCard({'name':'Tayto',
+    'img':'https://www.irishtimes.com/polopoly_fs/1.4078148!/image/image.jpg',
+    'description':'Cheese & Onion Chips'})
---
+++ public/sources/pages/productos/validations.js
@@ -0,0 +1,23 @@
+/** Validaciones
+ * Tenemos que validar que el precio sea un número positivo mayor a 0
+ * El precio no debe contener letras, ni simbolos especiales
+ * El stock debe ser mayor a 0 y estar dentro de un rango de 1000
+ * Que el nombre no sea muy largo y sea una cadena
+ * 
+*/
+
+
+const validateFields = (name, price, stock) =>{
+    let mensaje = false;
+    if(name.match(/^[A-Za-zÁÉÍÓÚáéíóúÑñ ]{1,30}$/)){
+        if(typeof(price) === "number" && (price > 0 && price < 1000) ){
+            if(typeof(stock) === "number" && (stock > 0 && stock < 1000)){
+                mensaje = true;
+            }
+        }
+    } 
+    console.log(mensaje ? "Los datos son válidos." : "Los datos no son válidos.");
+    return mensaje;
+};
+
+export{validateFields};
