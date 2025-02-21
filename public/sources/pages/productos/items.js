23d1a6378d73e2833ee7ee1472a19bcd8a3abf56
Jael Bohorquez <Jaelbkz@icloud.com>
2025-02-21 09:24:10 -0600

    se añade boton de eliminar card y se da funcionalidad

--- public/sources/pages/productos/items.js
+++ public/sources/pages/productos/items.js
@@ -20,10 +20,12 @@ console.log("Productos cargados:", itemsController.items);
     document.getElementById("list-items").innerHTML += itemHTML;
 } */
 
-    function addItemCard(item) {
-        const itemHTML = `
-           <!-- <div class="card" style="width: 20rem; margin-bottom: 20px;">
-                <img src="${item.imageUrl}" class="card-img-top" style="height: 250px; object-fit: cover;" alt="image">
+function addItemCard(item) {
+	const itemHTML = `
+            <!-- <div class="card" style="width: 20rem; margin-bottom: 20px;">
+                <img src="${
+					item.imageUrl
+				}" class="card-img-top" style="height: 250px; object-fit: cover;" alt="image">
                 <div class="card-body">
                     <h5 class="card-title">${item.name}</h5>
                     <p class="card-text">${item.description}</p>
@@ -31,28 +33,39 @@ console.log("Productos cargados:", itemsController.items);
             </div> -->
             <div class="col-10 col-md-3 p-1">
                 <div class="card" >                  
-                        <img src="${item.imageUrl}" class="card-img-top product-img img-thumbnail">
+                        <img src="${
+							item.imageUrl
+						}" class="card-img-top product-img img-thumbnail">
                     <div class="card-body">
                         <div class="card-title-container">
                             <h5 title="${item.name}}" class="card-nombre">
-                            ${item.name.length > 50 ? item.name.substring(0, 47) + "..." : item.name}
+                            ${
+								item.name.length > 50
+									? item.name.substring(0, 47) + "..."
+									: item.name
+							}
                             </h5>
                         </div>
-                        <p class="card-text" id="texto-precio"><strong>Precio: $${item.price}</strong></p>
+                        <p class="card-text" id="texto-precio"><strong>Precio: $${
+							item.price
+						}</strong></p>
                     </div>
                     <div class="card-footer">
-                    <button class="btn btn-primary" id="texto-boton"> Agregar a la canasta</a>
+                    <button class="btn btn-primary" id="texto-boton"> Agregar a la canasta</button>
+                    <button class="btn btn-primary mt-1" id="deleteBtn">Eliminar</button>
+                    <button class="btn btn-primary mt-1" id="deleteBtn">Modificar</button>
                     </div>
                 </div>
             </div>
         `;
-        
-        document.getElementById("list-items").insertAdjacentHTML("beforeend", itemHTML);
-    }
 
+	document
+		.getElementById("list-items")
+		.insertAdjacentHTML("beforeend", itemHTML);
+}
 
 // Mostrar los ítems almacenados
 
-itemsController.items.forEach(item => {
-    addItemCard(item);
+itemsController.items.forEach((item) => {
+	addItemCard(item);
 });
